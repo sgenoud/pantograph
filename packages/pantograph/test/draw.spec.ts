7443516100881f78d6c255f49f90a679fe8ab5af
Steve Genoud <steve@sgenoud.com>
2023-05-25 11:15:58 +0200

    Add ellipse methods to the draw API

--- packages/pantograph/test/draw.spec.ts
+++ packages/pantograph/test/draw.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { Vector } from "../src/definitions";
 import { draw } from "../src/draw";
+//import { debugImg } from "./debug";
 
 import { rect, polygon } from "./quickShapes";
 
@@ -52,5 +53,21 @@ describe("draw", () => {
         [3.6341463414634148, 0.7073170731707324],
       ])
     );
+  });
+
+  it("should draw half ellipses as expected", () => {
+    const fig = draw([0, 0]).halfEllipseTo([2, 2], 0.5).vLine(-2).close();
+    //debugImg(fig);
+    expect(fig).toMatchSnapshot();
+
+    const fig2 = draw([0, 0]).halfEllipseTo([2, 2], 5).vLine(-2).close();
+    //debugImg(fig2);
+    expect(fig2).toMatchSnapshot();
+  });
+
+  it("should close with a half circle without issue ", () => {
+    const fig = draw([0, 0]).bulgeArcTo([2, 2], 1).close();
+    //debugImg(fig);
+    expect(fig).toMatchSnapshot();
   });
 });
