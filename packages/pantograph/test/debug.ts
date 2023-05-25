305a47be4a7fc6f66d231ee7bc0213b97ecbb4a6
Steve Genoud <steve@sgenoud.com>
2023-05-25 11:00:00 +0200

    Testing and debugging helpers improvements

--- packages/pantograph/test/debug.ts
+++ packages/pantograph/test/debug.ts
@@ -3,6 +3,9 @@ import { exportSVG } from "../src/export/svg/exportSVG";
 
 import fs from "node:fs";
 import { fileURLToPath } from "node:url";
+import { BoundingBox } from "../src/main";
+import { polygon } from "./quickShapes";
+import { drawCircle } from "../src/drawShape";
 
 // There is a junk drawer of function that I use for debugging.
 
@@ -27,7 +30,21 @@ export const debugImg = (figures: any, name = "show", dir = "./__debug__") => {
   fs.writeFileSync(fileURLToPath(fileURL), svg);
 };
 
+export const dpnt = (point: any, radius = 0.05) => {
+  return drawCircle(radius).translateTo(point);
+};
+
 export const printPolygon = (p: any) =>
   console.log(
     `polygon(${JSON.stringify(p.segments.map((s: any) => s.firstPoint))})`
   );
+
+export const drawBbox = (shape: { boundingBox: BoundingBox }) => {
+  const { xMin, yMin, xMax, yMax } = shape.boundingBox;
+  return polygon([
+    [xMin, yMin],
+    [xMax, yMin],
+    [xMax, yMax],
+    [xMin, yMax],
+  ]);
+};
